538b3cccc5ab2b19bb3dd42af66ff6942749ba25
nata-makhanko <89252813379z@gmail.com>
2023-04-02 21:07:07 +0300

    added settings package.json and index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,9 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./components/app/app";
-import { BrowserRouter as Router } from 'react-router-dom';
-// import { createStore, compose, applyMiddleware } from 'redux';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from "./services/store";
-// import { rootReducer } from './services/reducers';
-// import thunk from 'redux-thunk';
-
-// declare global {
-//   interface Window {
-//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//   }
-// }
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const enhancer = composeEnhancers(applyMiddleware(thunk));
-// const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <Router>
